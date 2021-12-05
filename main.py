86404acd176dd2b94eff8fe13b2bf471e8bd7e6f
magisystem0408 <masato.matsudo@gmail.com>
2021-12-06 02:24:14 +0900

    add pathParam

--- main.py
+++ main.py
@@ -1,4 +1,15 @@
+from fastapi import FastAPI
+
+app = FastAPI()
 
 
+# オペレーションという
+@app.get("/")
+async def index():
+    return {"message": "hello world"}
 
-
+# パスパラメータ
+@app.get("/countries/{country_name}")
+# 型ヒントを入れるとバリテーションできる
+async def index(country_name: str):
+    return {"message": country_name}
