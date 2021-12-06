1f8feb5657393f5169b5fb503b9d5ad9f5e8d91f
magisystem0408 <masato.matsudo@gmail.com>
2021-12-06 11:07:58 +0900

    queryParam

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+from typing import Optional
 from fastapi import FastAPI
+
 
 app = FastAPI()
 
@@ -8,8 +10,20 @@ app = FastAPI()
 async def index():
     return {"message": "hello world"}
 
+
 # パスパラメータ
 @app.get("/countries/{country_name}")
 # 型ヒントを入れるとバリテーションできる
 async def index(country_name: str):
     return {"message": country_name}
+
+
+# クエリパラメータ
+@app.get("/countries/")
+# Optionalをつけると必須でなくても良いことになる。
+async def country(country_name: Optional[str] = None, country_no: Optional[int] = None):
+    return {
+        "country_name": country_name,
+        "country_no": country_no,
+    }
+
